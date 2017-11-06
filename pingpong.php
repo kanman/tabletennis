c29cc61bb6cae52132f6eac8c252169192d0d904
“Andrew <andrewgnew@gmail.com>
2017-11-06 22:45:09 +1100

    More updates and code clean up

--- pingpong.php
+++ pingpong.php
@@ -2,7 +2,7 @@
 //Creating a table tennis game. 
 
 
-//1. Create Variables
+//1. Create Game Variables
 
 $player1 = 0;
 $player2 = 0;
@@ -12,6 +12,8 @@ $round = 0;
 //2. Specify game conditions
 //WIN - Player must reach a score of 11. 
 //Player must be a minimum of 2 points higher than opponent.
+
+echo "<h1>Table Tennis</h1>\n";
 
 while (abs($player1 - $player2)<2 || ($player1 <11 && $player2 <11)) {
 $round++;
@@ -25,7 +27,16 @@ $player2++;
 
 echo "Player1 = $player1<br/>\n";
 echo "Player2 = $player2<br/>\n";
-
 }
+
+echo "<h2>";
+if ($player1 > $player2) {
+echo "Congratulations Player 1!";
+} else {
+    
+echo "Congratulations Player 2!";    
+}
+
+echo " You are the winner after $round rounds.</h2>\n"
 
 ?>
